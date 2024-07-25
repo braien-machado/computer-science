9e7328e8ba453cc7b10b6d66a9ec8d7c0572410c
Braien Machado de Paula <braienmp@outlook.com>
2024-07-24 22:19:35 -0300

    feat: create main class and enable starting program

---
+++ programmingone/objectsandclasses/CourseManagementSystem.java
@@ -0,0 +1,9 @@
+package programmingone.objectsandclasses;
+
+public class CourseManagementSystem {
+  public static void main(String[] args) {
+    System.out.println("Demo UoPeople Course Enrollment and Grade Management System (CEGMS)");
+
+    AdministratorInterface.startProgram();
+  }
+}
